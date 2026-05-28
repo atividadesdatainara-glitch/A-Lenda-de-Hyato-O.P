c908cb1a9366a17091a2cf1f2fe4ea07e0f037cc
atividadesdatainara-glitch <atividadesdatainara@gmail.com>
2026-05-28 16:30:35 -0300

    Goal pronto

--- cenas_sprites/gool/color_rect.gd
+++
@@ -1,5 +0,0 @@
-extends ColorRect
-
-var threshold = 0.0
-func _process(delta):
-	material.set("shader_parameter/threshold", threshold)
--- scripts/goal/color_rect.gd
+++ scripts/goal/color_rect.gd
@@ -1,11 +1,5 @@
 extends ColorRect
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var threshold = 0.0
+func _process(delta):
+	material.set("shader_parameter/threshold", threshold)
